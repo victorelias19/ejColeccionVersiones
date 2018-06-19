addf40b086d5a6197c775c033d3860e891e8f602
Víctor <Vctor@Victor-PC>
2018-06-19 11:38:48 +0200

    Commit 1

---
+++ src/indexMain.java
@@ -0,0 +1,14 @@
+
+public class indexMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Set coleccionSet = new HashSet();
+		coleccionSet.add("uno");
+		
+		}
+
+}
